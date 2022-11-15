dc8e6ec6d5dddddea1ce756a1c9f01e6343cfd43
liguangyi <252438823@qq.com>
2022-11-15 21:56:46 +0800

    setup basic redux project folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
+const mapStateToProps = (state) => {
+  return { counter: state.counter };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => {
+      dispatch({
+        type: 'INCREMENT'
+      })
+    },
+  }
+}
+
+function App(props) {
+  const {onIncrement, counter} = props
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <button onClick={onIncrement}>Click</button>
     </div>
   );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': return { counter: state.counter + 1 };
+    default: return state;
+  }
+}
+
+const store = createStore(reducer, { counter: 0 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
